86720631bf2645cd925e644c33ef90e01a3aaf90
Nguyen Tien Nam <namnguyen23092003123@gmail.com>
2026-03-24 18:20:36 +0700

    new update

---
+++ src/compoents/Admin/SideBar.js
@@ -0,0 +1,95 @@
+import 'react-pro-sidebar/dist/css/styles.css';
+import {
+    ProSidebar,
+    Menu,
+    MenuItem,
+    SubMenu,
+    SidebarHeader,
+    SidebarFooter,
+    SidebarContent,
+} from 'react-pro-sidebar';
+
+import { FaTachometerAlt, FaGem, FaList, FaGithub, FaRegLaughWink, FaHeart } from 'react-icons/fa';
+import sidebarBg from '../../assets/bg2.jpg';
+
+import { DiReact } from "react-icons/di"
+import { MdDashboard } from "react-icons/md"
+
+const SideBar = (props) => {
+    const { image, collapsed, toggled, handleToggleSidebar } = props;
+    return (
+        <>
+            <ProSidebar
+                image={sidebarBg}
+                collapsed={collapsed}
+                toggled={toggled}
+                breakPoint="md"
+                onToggle={handleToggleSidebar}
+            >
+                <SidebarHeader>
+                    <div
+                        style={{
+                            padding: '24px',
+                            textTransform: 'uppercase',
+                            fontWeight: 'bold',
+                            fontSize: 14,
+                            letterSpacing: '1px',
+                            overflow: 'hidden',
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                        }}
+                    >
+                        <DiReact size={'3em'} color={'00bfff'} />
+                        <span> Nam Dev</span>
+                    </div>
+                </SidebarHeader>
+
+                <SidebarContent>
+                    <Menu iconShape="circle">
+                        <MenuItem
+                            icon={<MdDashboard />}
+                            suffix={<span className="badge red">New</span>}
+                        >
+                            dashboard
+                        </MenuItem>
+
+                    </Menu>
+                    <Menu iconShape="circle">
+                        <SubMenu
+                            icon={<FaGem />}
+                            title="Features"
+                        >
+                            <MenuItem> Quản lý users</MenuItem>
+                            <MenuItem> Quản lý bài quiz</MenuItem>
+                            <MenuItem> Quản lý câu hỏi</MenuItem>
+                        </SubMenu>
+
+                    </Menu>
+                </SidebarContent>
+
+                <SidebarFooter style={{ textAlign: 'center' }}>
+                    <div
+                        className="sidebar-btn-wrapper"
+                        style={{
+                            padding: '20px 24px',
+                        }}
+                    >
+                        <a
+                            href="https://github.com/a51ad"
+                            target="_blank"
+                            className="sidebar-btn"
+                            rel="noopener noreferrer"
+                        >
+                            <FaGithub />
+                            <span style={{ whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden' }}>
+                                Made by Nguyen Tien Nam
+                            </span>
+                        </a>
+                    </div>
+                </SidebarFooter>
+            </ProSidebar>
+        </>
+    )
+}
+
+export default SideBar;
--- src/compoents/Home/HomePage.js
+++ src/compoents/Home/HomePage.js
@@ -1,7 +1,20 @@
+import videoHomePage from "../../assets/video-homepage.mp4"
+
 const HomePage = (props) => {
     return (
-        <div className="home-page-container">
-            HomePage
+        <div className="homepage-container">
+            <video autoPlay loop muted >
+                <source src={videoHomePage} type="video/mp4" />
+            </video>
+            <div className="homepage-content">
+                <div className="title-1">Welcome to my project - Nam Dev</div>
+                <div className="title-2">
+                    This is a project that I created to practice ReactJS and Redux. I hope you like it.
+                </div>
+                <div className="title-3">
+                    <button>Get Started</button>
+                </div>
+            </div>
         </div>
     )
 }
